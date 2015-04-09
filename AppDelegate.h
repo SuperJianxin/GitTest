a7e9013e404401306eec81f2f9532ec31015fd59
lijianxin <1824601515@qq.com>
2015-04-09 17:07:22 +0800

    Add file

---
+++ AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iFamily
+//
+//  Created by letianpai1 on 15/1/27.
+//  Copyright (c) 2015年 SpiralBird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "IFInterfaceErrorObserver.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) IFInterfaceErrorObserver *interfaceErrorObserver;
+
++ (AppDelegate *)shareAppdalegate;
+
+//打开主界面
+- (void) openMainView;
+//打开登录界面
+- (void) openLoginView;
+
+@end
+
